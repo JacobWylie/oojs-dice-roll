59c5d6caa2fc25e83c4191adb6a4b9e0eb2362b9
JacobWylie <jacobwylie@gmail.com>
2017-02-27 14:11:33 +0100

    introduced 'this' to call to dice method 'sides'

--- dice.js
+++ dice.js
@@ -1,7 +1,15 @@
 var dice = {
-	roll: function diceRoll() {
-  		var sides = 6;
-  		var randomNumber = Math.floor(Math.random() * sides) + 1;
+	sides: 6,
+	roll: function () {
+  		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+  		console.log(randomNumber);
+	}
+}
+
+var diceTen = {
+	sides: 10,
+	roll: function () {
+  		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
   		console.log(randomNumber);
 	}
 }
