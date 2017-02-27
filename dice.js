a902cae8d2662ecb8028bb6b9aacfca4edc63353
JacobWylie <jacobwylie@gmail.com>
2017-02-27 15:18:55 +0100

    Changed object to a constructor function

--- dice.js
+++ dice.js
@@ -1,16 +1,26 @@
-var dice = {
-	sides: 6,
-	roll: function () {
+// var dice = {
+// 	sides: 6,
+// 	roll: function () {
+//   		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//   		console.log(randomNumber);
+//   		return randomNumber;
+// 	}
+// }
+
+// var diceTen = {
+// 	sides: 10,
+// 	roll: function () {
+//   		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+//   		console.log(randomNumber);
+// 	}
+// }
+
+function Dice(sides) {
+	this.sides = sides;
+	this.roll = function () {
   		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-  		console.log(randomNumber);
   		return randomNumber;
-	}
+  	}
 }
 
-var diceTen = {
-	sides: 10,
-	roll: function () {
-  		var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-  		console.log(randomNumber);
-	}
-}
+var dice = new Dice(6);
